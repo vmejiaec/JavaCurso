8353e61df68dbb71e12a13673a9bbb25c1057cf6
Víctor Mejía <vmejiaec@gmail.com>
2026-05-11 00:06:53 -0500

    Las pruebas de operaciones sencillas

--- src/main/java/victor/farmacia/App.java
+++ src/main/java/victor/farmacia/App.java
@@ -3,5 +3,7 @@ package victor.farmacia;
 public class App{
     public static void main (String[] ags){
         System.out.println("Hola desde farmacia app");
+        Operacion op = new Operacion();
+        System.out.print(op.sumar(1, 2));        
     }
 }
---
+++ src/main/java/victor/farmacia/Operacion.java
@@ -0,0 +1,9 @@
+package victor.farmacia;
+
+public class Operacion {
+    public int sumar(int a, int b){
+        int res;
+        res = a + b;
+        return res;
+    }
+}
